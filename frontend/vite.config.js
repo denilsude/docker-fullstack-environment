ed545128bb95498725e3d3cfa9663d4f390699c7
denilson oliveira <denilsonfb21@gmail.com>
2025-09-24 00:16:27 -0300

    fix: Atualiza config do Vite para permitir proxy do Nginx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ADICIONE ESTE BLOCO ABAIXO
+  server: {
+    // Isso faz o servidor escutar em todas as interfaces de rede,
+    // o que é necessário para o Docker.
+    host: true,
+    // Aqui está a correção para o erro que você viu.
+    // Permitimos que o Nginx (visto como host 'frontend') se conecte.
+    allowedHosts: ['frontend'],
+  }
 })
